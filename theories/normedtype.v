c6e1919c6b0e33fc5e1236c1f64153dea9212de2
mkerjean <kerjean@lipn.fr>
2024-02-23 18:04:36 +0100

    evt wip

--- theories/normedtype.v
+++ theories/normedtype.v
@@ -147,60 +147,6 @@ Import numFieldTopology.Exports.
 Local Open Scope classical_set_scope.
 Local Open Scope ring_scope.
 
-HB.structure Definition PointedNmodule := {M of Pointed M & GRing.Nmodule M}. 
-HB.structure Definition PointedZmodule := {M of Pointed M & GRing.Zmodule M}. 
-HB.structure Definition FilteredNmodule := {M of Filtered M M & GRing.Nmodule M}.
-HB.structure Definition FilteredZmodule := {M of Filtered M M & GRing.Zmodule M}.
-HB.structure Definition NbhsNmodule := {M of Nbhs M & GRing.Nmodule M}.
-HB.structure Definition NbhsZmodule := {M of Nbhs M & GRing.Zmodule M}.
-HB.structure Definition TopologicalNmodule := {M of Topological M & GRing.Nmodule M}.
-HB.structure Definition TopologicalZmodule := {M of Uniform M & GRing.Zmodule M}.
-
-(* TODO: put this notation in classical_sets.v
-   and clean in topology.v cantor.v *)
-Local Notation "A ^-1" := ([set xy | A (xy.2, xy.1)]) : classical_set_scope.
-
-Definition convex (R : ringType) (M : lmodType R) (A : set M) :=
-  forall x y lambda, lambda *: x + (1 - lambda) *: y \in A. 
-
-HB.mixin Record Uniform_isEvt (R : numDomainType) E of Uniform E & GRing.Lmodule R E := {
-    add_continuous : continuous (uncurry (@GRing.add E));
-    scale_continuous : continuous (uncurry (@GRing.scale R E) : R^o * E -> E);
-    locally_convex : exists B : set (set E), (forall b, b \in B -> convex b) /\ (basis B)
-  }.
-
-HB.structure Definition Evt (R : numDomainType) :=
-  {E of Uniform_isEvt R E & Uniform E & GRing.Lmodule R E}. 
-
-HB.factory Record TopologicalLmod_isEvt (R : numDomainType) E
-      of Topological E & GRing.Lmodule R E := {
-    add_continuous : continuous (uncurry (@GRing.add E));
-    scale_continuous : continuous (uncurry (@GRing.scale R E) : R^o * E -> E);
-    locally_convex : exists B : set (set E), (forall b, b \in B -> convex b) /\ (basis B)     
-  }.
-HB.builders Context R E of TopologicalLmod_isEvt R E.
-  Definition entourage : set_system (E * E). Admitted.
-  Lemma entourage_filter : Filter entourage. Admitted.
-  Lemma entourage_refl_subproof :
-        forall A : set (E * E), entourage A -> [set xy | xy.1 = xy.2] `<=` A.
-  Admitted.
-  Locate "^-1".
-  Lemma entourage_inv_subproof :
-        forall A : set (E * E), entourage A -> entourage A^-1%classic.
-  Admitted.
-  Lemma entourage_split_ex_subproof :
-        forall A : set (E * E),
-        entourage A -> exists2 B : set (E * E), entourage B & B \; B `<=` A.
-  Admitted.
-  Lemma nbhsE_subproof : nbhs = nbhs_ entourage.
-  Admitted.
-
-  HB.instance Definition _ := Nbhs_isUniform_mixin.Build E
-    entourage_filter entourage_refl_subproof
-    entourage_inv_subproof entourage_split_ex_subproof
-    nbhsE_subproof.
-HB.end.
-
 Section limf_esup_einf.
 Variables (T : choiceType) (X : filteredType T) (R : realFieldType).
 Implicit Types (f : X -> \bar R) (F : set (set X)).
